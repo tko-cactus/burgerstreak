b6f342872b99b7e05fe9af5b8f02e5796ddb83b5
tko-cactus <37374201+tko-cactus@users.noreply.github.com>
2022-05-05 21:48:54 +0900

    create NavigationBar component

---
+++ src/components/layouts/HaaderOnly.tsx
@@ -0,0 +1,16 @@
+import { ReactNode, VFC } from "react";
+import { NavigationBar } from "../organisms/NavigationBar";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const HeaderOnly: VFC<Props> = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <NavigationBar />
+      {children}
+    </>
+  );
+};
